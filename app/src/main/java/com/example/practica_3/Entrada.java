435f0a1cee64218d9cc3d4a85d6688b67538860a
DESKTOP-6BEIRMS\Sergio <sergiosabater@gmail.com>
2021-03-08 21:40:35 +0100

    WIP: Added new features

--- app/src/main/java/com/example/practica_3/Entrada.java
+++ app/src/main/java/com/example/practica_3/Entrada.java
@@ -2,14 +2,18 @@ package com.example.practica_3;
 
 public class Entrada {
 
+    //Variables
+
     public final String titular;
     public final String autor;
     public final String link;
+    private String nombre;
 
     public Entrada(String titular, String autor, String link){
         this.titular = titular;
         this.autor = autor;
         this.link = link;
+        this.nombre = "Json";
     }
 
     public String getTitular() {
